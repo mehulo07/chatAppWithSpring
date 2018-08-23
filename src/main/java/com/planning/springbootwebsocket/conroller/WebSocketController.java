122975f0397b2e5624f106e8159385f60bae7ce7
Mehul Makwana <mehulmakwana339@gmail.com>
2018-08-23 11:57:12 +0530

    initial Commit

---
+++ src/main/java/com/planning/springbootwebsocket/conroller/WebSocketController.java
@@ -0,0 +1,29 @@
+package com.planning.springbootwebsocket.conroller;
+
+import java.util.HashMap;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+import com.planning.springbootwebsocket.model.ChatMessage;
+
+@Controller
+public class WebSocketController {
+	
+	@MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/publicChatRoom")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        return chatMessage;
+    }
+	@MessageMapping("/chat.addUser")
+	@SendTo("/topic/publicChatRoom")
+	public ChatMessage addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+		
+		headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+   return chatMessage;
+	    }
+		
+}
