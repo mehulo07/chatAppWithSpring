235c82a00937cbdb4d320e0d6b096b324419aca0
Mehul Makwana <mehulmakwana339@gmail.com>
2018-08-23 12:11:00 +0530

    main controller commit.

--- src/main/java/com/planning/springbootwebsocket/conroller/MainController.java
+++ src/main/java/com/planning/springbootwebsocket/conroller/MainController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
-
+	/*main controller comment*/
 	  @RequestMapping("/")
 	    public String index(HttpServletRequest request, Model model) {
 	        String username = (String)request.getSession().getAttribute("username");
